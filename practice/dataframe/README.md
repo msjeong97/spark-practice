7b8aabfc9631f126d3c0b02a9d7d7c3941404af0
Minseop Jeong <hpmltsws@gmail.com>
2021-11-27 14:16:58 +0900

    feature: practice spark df api2

--- practice/dataframe/README.md
+++ practice/dataframe/README.md
@@ -28,10 +28,40 @@ df.filter(col("count") > 100)
 df.where("ORIGIN_COUNTRY_NAME == \"United States\"")
 df.where("count > 100")
 ```
+- SQL-like하지 않게 사용도 가능
+```scala
+df.where(col("ORIGIN_COUNTRY_NAME") === "United States")
+df.where(col("ORIGIN_COUNTRY_NAME") =!= "United States")
+```
 
 ## orderBy
 - column 조건에 따라 정렬된 dataframe을 리턴
 - `desc`, `asc`를 사용 가 
 ```scala
 df.where("count > 100").orderBy(desc("count"))
+```
+
+## select
+- sql과 마찬가지로 특정 column을 select한 dataframe을 리턴
+```scala
+df.select("DEST_COUNTRY_NAME")
+df.select("DEST_COUNTRY_NAME", "ORIGIN_COUNTRY_NAME")
+```
+- column을 사용할 때(select, where, ...) 아래와 같이 column을 사용할 수 있음적
+```scala
+df.select("DEST_COUNTRY_NAME") // spark의 implicit 변환(string -> column)이 적용됨
+df.select($"DEST_COUNTRY_NAME")
+df.select(col("DEST_COUNTRY_NAME"))
+```
+
+## distinct
+- sql과 마찬가지로 구분되는 row들의 집합(dataframe)을 리턴
+```scala
+df.select("DEST_COUNTRY_NAME").distinct()
+```
+
+## union
+- sql과 마찬가지로 두 dataframe을 union한 dataframe을 리턴
+```scala
+df1.union(df2)
 ```
--- practice/dataframe/SparkDFApi.scala
+++ practice/dataframe/SparkDFApi.scala
@@ -1,6 +1,7 @@
 // scalastyle:off println
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.implicits._
 
 object SparkDFApi extends Serializable {
   def main(args: Array[String]): Unit = {
@@ -10,28 +11,42 @@ object SparkDFApi extends Serializable {
       .getOrCreate()
     spark.conf.set("spark.sql.shuffle.partitions", "5")
 
-    val flightData = spark.read
+    val df = spark.read
       .format("json")
       .load("/Users/minseop/minseop/spark/data/flight-data/json/2015-summary.json")
 
-    val df_new_column_added = flightData.withColumn("count_as_string", col("count").cast("string"))
+    val df_new_column_added = df.withColumn("count_as_string", col("count").cast("string"))
     // add new column which is casted to string from "count"
     println(added_new_column.schema)
 
-    val df_column_renamed = flightData.withColumnRenamed("DEST_COUNTRY_NAME", "dest")
+    val df_column_renamed = df.withColumnRenamed("DEST_COUNTRY_NAME", "dest")
     // dest ORIGIN_COUNTRY_NAME count
     println(df_column_renamed.columns.mkString(" "))
 
-    val filtered_1_1 = flightData.filter(col("ORIGIN_COUNTRY_NAME") === "United States")
-    val filtered_1_2 = flightData.where("ORIGIN_COUNTRY_NAME == \"United States\"")
+    val filtered_1_1 = df.filter(col("ORIGIN_COUNTRY_NAME") === "United States")
+    val filtered_1_2 = df.where("ORIGIN_COUNTRY_NAME == \"United States\"")
     // true
     println(filtered_1_1.count == filtered_1_2.count)
 
-    val filtered_2_1 = flightData.filter(col("count") > 100).orderBy(desc("count"))
-    val filtered_2_2 = flightData.where("count > 100").orderBy(desc("count"))
+    val filtered_2_1 = df.filter(col("count") > 100).orderBy(desc("count"))
+    val filtered_2_2 = df.where("count > 100").orderBy(desc("count"))
     // same result
     println(filtered_2_1.show(10))
     println(filtered_2_2.show(10))
+
+    val df_distinct_dest_country = df.select("DEST_COUNTRY_NAME").distinct()
+    println(df_distinct_dest_country.count())
+
+    val df1 = spark.read
+      .format("json")
+      .load("/Users/minseop/minseop/spark/data/flight-data/json/2015-summary.json")
+    val df2 = spark.read
+      .format("parquet")
+      .load("/Users/minseop/minseop/spark/data/flight-data/parquet/2010-summary.parquet")
+    val df_filtered = df1.union(df2).where($"ORIGIN_COUNTRY_NAME" =!= "United States")
+    println(df_filtered.count())
+
+
 
 
 
